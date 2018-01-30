b17d1707e05a3e5532dfdfb90a591c307d1d3468
Poulad <poulad.pld@gmail.com>
2018-01-30 00:01:34 -0500

    Add island and tank

---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class Shell : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    public void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject, 2);
+        _audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+public class Tank : MonoBehaviour
+{
+    public Rigidbody2D Shell;
+
+    private Rigidbody2D _rigidbody;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private AudioSource _audioSource;
+
+    private bool _fired;
+
+    public void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void FixedUpdate()
+    {
+        _audioSource.pitch = Random.Range(.9f, 1.1f);
+
+        float moveH = Input.GetAxis("Horizontal");
+        if (0 < Mathf.Abs(moveH))
+        {
+            _rigidbody.velocity = new Vector2(moveH, _rigidbody.velocity.y);
+            bool spriteIsInRightDirection = (moveH > 0 && !_spriteRenderer.flipX) ||
+                                            (moveH < 0 && _spriteRenderer.flipX);
+            if (!spriteIsInRightDirection)
+                _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        }
+
+        if (!_fired && Input.GetKey(KeyCode.F))
+        {
+            //_fired = true;
+            var shellInstance = Instantiate(Shell, transform.position, Quaternion.identity);
+            shellInstance.velocity = 2.5f * (_spriteRenderer.flipX ? Vector3.left : Vector3.right);
+        }
+    }
+}
