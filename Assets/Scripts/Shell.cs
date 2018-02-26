03607c9dd7abaddc3ef73c8ca5af4f8c70ce7a5a
Poulad <poulad.pld@gmail.com>
2018-02-26 13:03:46 -0500

    m

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 // ReSharper disable once CheckNamespace
 public class Shell : MonoBehaviour
@@ -15,8 +18,37 @@ public class Shell : MonoBehaviour
         Destroy(gameObject, 3);
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
+        {
+            var tileMap = other.GetComponent<Tilemap>();
+            if (tileMap == null) return;
+        }
+
+
+        return;
+        {
+            var tileMap = other.GetComponent<Tilemap>();
+            if (tileMap == null) return;
+            var bounds = new BoundsInt(-2, -2, -1, 4, 4, 4);
+            var tiles = tileMap.GetTilesBlock(bounds);
+            tiles = tiles.Where(_ => _ != null).ToArray();
+            foreach (var t in tiles)
+            {
+                var tt = (Tile)t;
+                //tileMap.SetTile(tt.gameObject);
+                try
+                {
+                    //DestroyImmediate(tt, true);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            return;
+        }
+
         Destroy(other.gameObject, 2);
         _audioSource.Play();
     }
