54ab7475380a23e117ec7f68d508df42cae50192
Poulad <poulad.pld@gmail.com>
2018-02-26 23:20:43 -0500

    Skip building via script

---
+++ build-webgl.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -logFile Temp/build-webgl-logs.txt -executeMethod WebGLBuilder.Build
--- deploy_gh-pages.sh
+++ deploy_gh-pages.sh
@@ -4,15 +4,13 @@ set -ex
 
 tankmania_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 
-if [ -d "./Build" ]
-then
-    rm -rv "./Build"
-fi
-"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode -logFile Temp/build-webgl-logs.txt -executeMethod WebGLBuilder.Build
+# rm -rfv "./Build"
+# powershell "cmd /C build-webgl.bat"
+
 
 cd "../Tank-Mania_gh-pages"
-rm -rv ./*
-cp -rv ../Tank-Mania/Build/* .
+rm -rfv ./*
+cp -rv "$tankmania_dir"/Build/* .
 
 
 git add .
