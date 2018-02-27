0a97ec648abc3f5b52770046d3e43c7f824b9dff
Poulad <poulad.pld@gmail.com>
2018-02-27 12:44:10 -0500

    Change turns in 2 secs

--- Assets/Scripts/Scene Management/SceneManagerBehavior.cs
+++ Assets/Scripts/Scene Management/SceneManagerBehavior.cs
@@ -8,6 +8,8 @@ namespace TankMania
     {
         public GameObject[] Tanks;
 
+        public GameObject[] TanksPrefabs;
+
         public GameObject VirtualCameraObject;
 
         private GameObject _currentTank;
@@ -17,6 +19,17 @@ namespace TankMania
         public void Start()
         {
             _virtualCamera = VirtualCameraObject.GetComponent<CinemachineVirtualCamera>();
+
+            {// ToDo Testing
+                Tanks = new[]
+                {
+                    Instantiate(TanksPrefabs[0], new Vector3(-1, 2, 0), Quaternion.identity),
+                    Instantiate(TanksPrefabs[1], new Vector3(1, 2, 0), Quaternion.identity),
+                };
+
+                Tanks[1].GetComponent<SpriteRenderer>().color = Color.red;
+            }
+
             AssignTurnToTank(Tanks[0]);
         }
 
@@ -24,12 +37,12 @@ namespace TankMania
         {
             _virtualCamera.Follow = tank.transform;
             var tankBehavior = tank.GetComponent<TankBehavior>();
-            tankBehavior.Fired += OnTankFired;
+            tankBehavior.Fired += OnCurrentTankFired;
             tankBehavior.TakeCurrentTurn();
             _currentTank = tank;
         }
 
-        private void OnTankFired(object sender, EventArgs eventArgs)
+        private void OnCurrentTankFired(object sender, EventArgs eventArgs)
         {
             Invoke("ChangeTanksTurn", 2);
         }
@@ -37,6 +50,7 @@ namespace TankMania
         private void ChangeTanksTurn()
         {
             DestroyObject(_currentTank.gameObject);
+            AssignTurnToTank(Tanks[1]);
         }
     }
 }
