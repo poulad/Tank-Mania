3eaf2479f04e9bdadf5ea7f679a9283d403dc67b
Andre Oliveira <andreoliveira.it@gmail.com>
2018-03-14 21:05:04 -0400

    First Menu Skeleton

---
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour {
+
+	public void Play()
+    {
+        SceneManager.LoadScene("Playground", LoadSceneMode.Single);
+    }
+
+    public void Help()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
