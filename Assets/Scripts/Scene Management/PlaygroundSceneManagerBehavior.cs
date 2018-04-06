9c41920c99d80706b5f1464d17735036c9fd38d2
Poulad <poulad.pld@gmail.com>
2018-04-06 01:04:56 -0400

    Enhance playground

--- Assets/Scripts/Scene Management/PlaygroundSceneManagerBehavior.cs
+++ Assets/Scripts/Scene Management/PlaygroundSceneManagerBehavior.cs
@@ -1,40 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace TankMania
 {
     public class PlaygroundSceneManagerBehavior : LevelSceneManagerBehavior
     {
-        public GameObject TankPrefab;
-
-#if UNITY_EDITOR
-        public new void Awake()
-        {
-            //var player1 = new Player("Player 1")
-            //{
-            //    TankPrefab = TankPrefab
-            //};
-            //var player2 = new Player("Player 2")
-            //{
-            //    TankPrefab = TankPrefab
-            //};
-
-            //GameManager.Current.Players = new[] { player1, player2 };
-            GameManager.ReturnToScene = Constants.Scenes.Playground;
-
-            base.Awake();
-        }
-#endif
-
         public new void Start()
         {
+            TurnTimeout = 30;
+            GameManager.Current.Players = GameManager.Current.Players.Take(2).ToArray();
+
             foreach (var player in AllPlayers)
             {
                 player.Tank = Instantiate(player.TankPrefab, transform);
                 player.Tank.name = "Tank - " + player.Name;
             }
 
-            AllPlayers[0].Tank.transform.position = new Vector3(-2, 2, 0);
-            AllPlayers[1].Tank.transform.position = new Vector3(2, 2, 0);
+            AllPlayers[0].Tank.transform.position = new Vector3(-3, 2, 0);
+            AllPlayers[1].Tank.transform.position = new Vector3(3, 2, 0);
 
             AssignTurnToPlayer(AllPlayers[0]);
 
