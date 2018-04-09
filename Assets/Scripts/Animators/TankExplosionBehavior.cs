7d416539150671be9fad7b42b439103cdc9d57a1
Poulad <poulad.pld@gmail.com>
2018-04-09 17:36:06 -0400

    Add more explosion sounds

--- Assets/Scripts/Animators/TankExplosionBehavior.cs
+++ Assets/Scripts/Animators/TankExplosionBehavior.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace TankMania
 {
     public class TankExplosionBehavior : MonoBehaviour
     {
+        public AudioClip[] AudioClips;
+
         public event EventHandler<EventArgs> Finished;
+
+        private AudioSource _audioSource;
+
+        public void Start()
+        {
+            _audioSource = GetComponentInParent<AudioSource>();
+
+            _audioSource.clip = AudioClips[Random.Range(0, AudioClips.Length)];
+            _audioSource.Play();
+        }
 
         public void FinishExplosion()
         {
